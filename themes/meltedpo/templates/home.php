1b6299d70bf5d647ce611c9e45bc1522eb7a4a24
Michael Bradshaw <michaelbradshaw@PGMichar216BUPB.byu.edu>
2017-07-20 17:05:20 -0600

    added my wp-content to the repo

---
+++ themes/meltedpo/templates/home.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>MeltedPopsicle</title>
+</head>
+<body>
+	<h1>Michael, I am you!</h1>
+</body>
+</html>
